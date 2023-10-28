f54646a412e909e7e305e92bb07398d2451ccb41
AmirHossein <Eliot_1_999@outlook.com>
2023-10-28 15:34:48 +0330

    all files added

---
+++ Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Footer() {
+  const isPassed = true;
+  return (
+    <div className='footer'>
+        <p>Course Resault</p>
+        {isPassed ? (
+        <p>
+          You have successfully passed <strong>33%</strong> of your courses.
+        </p>
+      ) : (
+        <p>You have not passed any course</p>
+      )}
+    </div>
+  )
+}
+
+export default Footer
